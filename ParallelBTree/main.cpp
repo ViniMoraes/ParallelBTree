3f37c759da544587d83165561ef069d2e7c50664
ViniciusMoraes <moraes.vinicius@hotmail.com>
2019-12-07 20:25:08 -0300

    Initial Commit

---
+++ ParallelBTree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ParallelBTree
+//
+//  Created by Vinicius Henrique Moraes on 07/12/2019.
+//  Copyright © 2019 Vinicius Henrique Moraes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
